e7cffb39df16292518be58f2223ecd53c639e232
zkahil <90580676+zkahil@users.noreply.github.com>
2026-08-14 03:04:10 +0100

    v1.7 fix delet espace problem

--- frontend-web/src/services/apiConfig.js
+++ frontend-web/src/services/apiConfig.js
@@ -2,6 +2,6 @@
 
 const API_BASE_URL = process.env.NODE_ENV === 'production'
     ? 'https://gestion-locataire-jack.vercel.app/'
-    : 'http://localhost:3000/api';
+    : 'http://localhost:3000';
 
 export default API_BASE_URL;
